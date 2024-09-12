abc3df934b7f09b6c44174ca1a4584e2ae037611
Contairsis <turdcicle06@gmail.com>
2024-09-12 15:44:20 -0700

    Update CarTester.java

--- CarTester.java
+++ CarTester.java
@@ -1,1 +1,40 @@
-
+import java.util.Scanner;
+public class CarTester{
+    Scanner carScanner = new Scanner(System.in);
+    String name ="";
+    int miles = 0;
+    int gallons = 0;
+    public static void main(String[] args){
+        CarTester tester = new CarTester();
+        Car car1 = tester.addCar();
+        System.out.println(car1.toString());
+        Car car2 = tester.addCar();
+        System.out.println(car2.toString());
+    }
+    public Car addCar(){
+        System.out.println("Name of car: ");
+        name = carScanner.next() + " " + carScanner.next();
+        System.out.println("Miles driven: ");
+        miles = carScanner.nextInt();
+        System.out.println("Gallons used: ");
+        gallons = carScanner.nextInt();
+        return new Car(name, miles, gallons);
+    }
+    
+}
+/* output of test
+Name of car: 
+Honda Civic
+Miles driven: 
+234
+Gallons used: 
+100
+Honda Civic averaged 2.3 m/g
+Name of car: 
+Toad Mobile
+Miles driven: 
+13
+Gallons used: 
+7
+Toad Mobile averaged 1.9 m/g
+ */
